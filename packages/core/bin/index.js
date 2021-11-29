17dd6fec48a99bd1a3ebb9b9db58f91793c44905
jshuaishuai <ajiang115@qq.com>
2021-11-29 13:31:27 +0800

    feat:修改了core

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -3,7 +3,7 @@
 const importLocal = require("import-local");
 
 if (importLocal(__dirname)) {
-    console.log("正在使用 @eit-monit 本地版本");
+    console.log("正在使用 @eit-monit 本地版本2222");
 } else {
     require("../lib")(process.argv.slice(2));
 }
