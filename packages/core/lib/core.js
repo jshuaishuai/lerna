a0ba711983304fa7609a05e00d6166a7defedc8a
jshuaishuai <ajiang115@qq.com>
2021-11-30 09:06:52 +0800

    feat:init

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,14 +1,15 @@
 // "use strict";
 
 // const _ = require("lodash");
-const behavior = require("@eit-monit/behavior");
+// const behavior = require("@eit-monit/behavior");
 // const colors = require("colors/safe"); // log 有颜色输出
 
 function core() {
     // TODO
     const obj = { name: "jason" };
     // console.log(colors.green(_.get(obj, "name")));
-    behavior();
+    // behavior();
+    console.log(obj.name);
 }
 
 module.exports = core;
